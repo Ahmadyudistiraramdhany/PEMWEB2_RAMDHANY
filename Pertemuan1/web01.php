cc8e5de208a88d39aa29c26cfdd694b04eb3a60c
Ahmadyudistiraramdhany <ahmadyudistiraramdhany@gmail.com>
2025-02-24 16:33:35 +0700

    Tugas & Praktikum

---
+++ Pertemuan1/web01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Web01</title>
+</head>
+<body>
+    <h1> Selamat Belajar PHP</h1>
+    <?php
+    $_nama = "Ahmad Yudistira Ramdhany";
+    $_umur = 21;
+    $_prodi = "Teknik Informatika";
+    $_ipk = 3.5;
+    ?>
+    <p>nama : <?php echo $_nama; ?></p>
+    <p>umur : <?php echo $_umur; ?></p>
+    <p>prodi : <?php echo $_prodi; ?></p>
+    <p>ipk : <?php echo $_ipk; ?></p>
+
+    <hr>
+    <?php
+        echo "Apa kabar $_nama"
+    ?>
+
+
+</body>
+</html> 
