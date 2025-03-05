dbe0c909db9a92207ed8e0b041059a40bee5d44d
Ahmadyudistiraramdhany <ahmadyudistiraramdhany@gmail.com>
2025-03-05 10:56:58 +0700

    Upload

---
+++ pertemuan3/belajarphp2025-main/admin/panggil.php
@@ -0,0 +1,19 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <?php 
+                    include_once 'menu.php';
+                ?>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Projek UTS</h1>
+                    <?php
+                    include_once 'projek_uts/login.html';
+                    ?>
+                </div>
+</div>
+<?php 
+    include_once 'bottom.php';
+?>
